41da1c03cf72733854e8741f68158b2cdecfb3b7
LuQuintas11 <luciaquintas14@gmail.com>
2022-07-12 14:36:34 +0000

    asdas

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,22 @@
  */
 
 // Regular function call 
+const sumAll= (a,b,c)=>a+b+c;
+let sum = sumAll(1,2,3);
+console.log(sum)
+
 
 // Extra arguments are ignored
+let sum2=sumAll(1,2,3,4,5,6,7)
+console.log(sum2)//sum
 
 // Function using ...rest
+const sumRest=(a,b,c,...rest)=>{
+    let sum=a+b+c;
+    for(let i of rest){
+        sum +=i
+    }
+    return sum
+}
+
+let sum3=sumRest(1,2,3,4,5,6)
